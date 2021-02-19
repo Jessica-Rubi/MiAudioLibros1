ad3a6becd8bbc7723b689d7afc7d954d1fb807a6
Patax <s16120277@alumnos.itsur.edu.mx>
2021-02-18 21:50:42 -0600

    Audio Libros ANTHONY

--- app/src/main/java/com/example/miaudiolibros/services/miIntentService.java
+++ app/src/main/java/com/example/miaudiolibros/services/miIntentService.java
@@ -1,17 +1,63 @@
 package com.example.miaudiolibros.services;
 
-import android.app.Service;
+import android.app.IntentService;
 import android.content.Intent;
-import android.os.IBinder;
+import android.os.AsyncTask;
+import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.JobIntentService;
 
-public class miIntentService extends JobIntentService {
+public class miIntentService extends IntentService {
+
+    public miIntentService() {
+        super("MiIntentSevice");
+    }
+
+    class MiTareaAsincrona extends AsyncTask<Integer, Integer, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //inicilizar recursos
+            Log.d("MIIS", "Se inicio el subproceso");
+        }
+
+        @Override
+        protected Boolean doInBackground(Integer... integers) {
+            Log.d("MIIS", "Se recibieron en el subproceso " + integers.length +" parametros" );
+
+            for (int i=0; i<integers.length ; i++){
+
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Log.d("MIIS", "Valor del parametro "+ (i+1) + " = " + integers[i]  );
+                publishProgress(i, i);
+            }
+
+            return integers.length>0;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            Log.d("MIIS", "Progreso en subproceso " + values[0] );
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if(aBoolean){
+                Log.d("MIIS", "Subproceso finalizado" );
+            }
+        }
+    }
 
     @Override
-    protected void onHandleWork(@NonNull Intent intent) {
-        
+    protected void onHandleIntent(@Nullable Intent intent) {
+
     }
 }
